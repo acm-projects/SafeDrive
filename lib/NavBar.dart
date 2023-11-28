110af020ccd44d2c5ef46ffe48d7c6dba5a341a4
Abhinav Malkoochi <abhinav.malkoochi@gmail.com>
2023-11-28 13:30:20 -0600

    integrated app:

--- lib/NavBar.dart
+++ lib/NavBar.dart
@@ -5,10 +5,9 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import './global.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'camera/object_detector_view.dart';
 
 class NavBar extends StatefulWidget {
-
   final int initialIndex;
   const NavBar({super.key, required this.initialIndex});
 
@@ -20,7 +19,6 @@ class _NavBarState extends State<NavBar> {
   int _pageIndex = 0; // Default value
 
   @override
-
   void initState() {
     super.initState();
     _pageIndex = widget.initialIndex;
@@ -31,7 +29,8 @@ class _NavBarState extends State<NavBar> {
     return Container(
       decoration: BoxDecoration(
           color: bgWhite,
-          borderRadius: const BorderRadius.only(topLeft: Radius.circular(15), topRight: Radius.circular(15)),
+          borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(15), topRight: Radius.circular(15)),
           boxShadow: [
             BoxShadow(
                 color: Colors.grey.shade900,
@@ -40,10 +39,8 @@ class _NavBarState extends State<NavBar> {
                 offset: const Offset(
                   5.0,
                   5.0,
-                )
-            )
-          ]
-      ),
+                ))
+          ]),
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
         child: GNav(
@@ -55,55 +52,48 @@ class _NavBarState extends State<NavBar> {
           activeColor: contrastAccentOne,
           tabBackgroundColor: backgroundAccentOne,
           padding: const EdgeInsets.all(20),
-
           tabs: [
             GButton(
               icon: Icons.home,
               text: 'Home',
               textStyle: GoogleFonts.poppins(
                   textStyle: const TextStyle(
-                    fontSize: 15,
-                    color: contrastAccentOne,
-                    fontWeight: FontWeight.bold,
-                  )
-              ),
-
+                fontSize: 15,
+                color: contrastAccentOne,
+                fontWeight: FontWeight.bold,
+              )),
             ),
             GButton(
               icon: Icons.videocam,
               text: 'Camera',
               textStyle: GoogleFonts.poppins(
                   textStyle: const TextStyle(
-                    fontSize: 15,
-                    color: contrastAccentOne,
-                    fontWeight: FontWeight.bold,
-                  )
-              ),
+                fontSize: 15,
+                color: contrastAccentOne,
+                fontWeight: FontWeight.bold,
+              )),
             ),
             GButton(
               icon: Icons.car_crash,
               text: 'Accident',
               textStyle: GoogleFonts.poppins(
                   textStyle: const TextStyle(
-                    fontSize: 15,
-                    color: contrastAccentOne,
-                    fontWeight: FontWeight.bold,
-                  )
-              ),
+                fontSize: 15,
+                color: contrastAccentOne,
+                fontWeight: FontWeight.bold,
+              )),
             ),
             GButton(
               icon: Icons.person,
               text: 'Profile',
               textStyle: GoogleFonts.poppins(
                   textStyle: const TextStyle(
-                    fontSize: 15,
-                    color: contrastAccentOne,
-                    fontWeight: FontWeight.bold,
-                  )
-              ),
+                fontSize: 15,
+                color: contrastAccentOne,
+                fontWeight: FontWeight.bold,
+              )),
             ),
           ],
-
           selectedIndex: _pageIndex,
           onTabChange: (index) {
             setState(() {
@@ -119,7 +109,7 @@ class _NavBarState extends State<NavBar> {
             } else if (_pageIndex == 1) {
               Navigator.of(context).push(
                 MaterialPageRoute(
-                  builder: (context) => CameraPage(),
+                  builder: (context) => ObjectDetectorView(),
                 ),
               );
             } else if (_pageIndex == 2) {
@@ -136,7 +126,6 @@ class _NavBarState extends State<NavBar> {
               );
             }
           },
-
         ),
       ),
     );
