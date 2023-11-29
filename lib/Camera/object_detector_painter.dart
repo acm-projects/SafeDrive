5728faadbfcc1710938813138648dca8ae1ca249
Abhinav Malkoochi <abhinav.malkoochi@gmail.com>
2023-11-28 18:51:14 -0600

    camera changes

--- lib/Camera/object_detector_painter.dart
+++ lib/Camera/object_detector_painter.dart
@@ -5,7 +5,7 @@ import 'dart:ui' as ui;
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_object_detection/google_mlkit_object_detection.dart';
-
+import 'package:flutter_beep/flutter_beep.dart';
 import 'coordinates_translator.dart';
 
 class ObjectDetectorPainter extends CustomPainter {
@@ -15,11 +15,22 @@ class ObjectDetectorPainter extends CustomPainter {
     this.rotation,
     this.cameraLensDirection,
   );
+  double left = 0.0;
+  double top = 0.0;
+  double right = 0.0;
+  double bottom = 0.0;
 
   final List<DetectedObject> _objects;
   final Size imageSize;
   final InputImageRotation rotation;
   final CameraLensDirection cameraLensDirection;
+  double getDistance() {
+    //(focal length(mm) * real height of object(mm) * image height(pixels))/ (object height (pixels) * sensor height(mm) )
+    if (bottom - top > 0) {
+      return ((23 * 1534 * 2340) / ((bottom - top) * 25.4));
+    }
+    return 0.0;
+  }
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -29,7 +40,7 @@ class ObjectDetectorPainter extends CustomPainter {
       ..color = Colors.lightGreenAccent;
 
     final Paint background = Paint()..color = Color(0x99000000);
-
+    double distance = getDistance();
     for (final DetectedObject detectedObject in _objects) {
       final ParagraphBuilder builder = ParagraphBuilder(
         ParagraphStyle(
@@ -42,39 +53,51 @@ class ObjectDetectorPainter extends CustomPainter {
       if (detectedObject.labels.isNotEmpty) {
         final label = detectedObject.labels
             .reduce((a, b) => a.confidence > b.confidence ? a : b);
-        //builder.addText('${label.text} ${label.confidence}\n');
+        /*
+        if (distance <= 3000.0 && distance != 0.0) {
+          FlutterBeep.beep();
+          builder.addText('$distance TOO CLOSE \n');
+        } else {*/
+        if (distance < 3000) {
+          FlutterBeep.beep();
+          builder.addText('$distance TOO CLOSE \n');
+        } else {
+          builder.addText('$distance\n');
+        }
+
+        //}
       }
       builder.pop();
 
-      final left = translateX(
+      left = translateX(
         detectedObject.boundingBox.left,
         size,
         imageSize,
         rotation,
         cameraLensDirection,
       );
-      final top = translateY(
+      top = translateY(
         detectedObject.boundingBox.top,
         size,
         imageSize,
         rotation,
         cameraLensDirection,
       );
-      final right = translateX(
+      right = translateX(
         detectedObject.boundingBox.right,
         size,
         imageSize,
         rotation,
         cameraLensDirection,
       );
-      final bottom = translateY(
+      bottom = translateY(
         detectedObject.boundingBox.bottom,
         size,
         imageSize,
         rotation,
         cameraLensDirection,
       );
-
+      distance = getDistance();
       canvas.drawRect(
         Rect.fromLTRB(left, top, right, bottom),
         paint,
